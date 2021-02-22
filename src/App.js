1779b079b5e7cdc2751bc84aedf16d4c18faaa4c
Nayem1315 <ndeveloper315@gmail.com>
2021-02-23 00:24:06 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name: "nayem",
+    or: "and",
+    work: "learn with jhankar mahbub"
+  };
+  const person2 = {
+    name: "mahfuz",
+    job: "singer"
+  };
+  const style = {
+    color: "black",
+    backgroundColor: "white"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +23,9 @@ function App() {
         <p>
           React App Open Successfully
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1>Muhtasim Islam Nayem</h1>
-        </a>
+        <h1 className="" style={style}>My Name Is {person.name + " " + person.or + " " + person.work}</h1>
+        <h3>My second jocker {person2.name + " " + person2.job}</h3>
+        <p>My first react paragraph</p>
       </header>
     </div>
   );
